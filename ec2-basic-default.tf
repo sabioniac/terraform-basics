1968b7241c9a4e3d9ddb736a3e525b8d2ad94f3c
sabioniac <sabionweb@gmail.com>
2025-10-16 20:41:10 +0000

    Initial Setup

---
+++ ec2-basic-default.tf
@@ -0,0 +1,21 @@
+#Terraform setting Block
+terraform{
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            #version = "~> 3.21" # Optional but recomended in production
+        }
+    }
+}
+
+#Provider Block
+provider "aws"{
+  profile = "default" #AWS credentials profile configured on your desktop terminal
+  region = "us-east-1"
+}
+
+#Resourec Block
+resource "aws_instance" "ec2demo"{
+  ami = "ami-0341d95f75f311023"
+  instance_type = "t2.micro"
+}
