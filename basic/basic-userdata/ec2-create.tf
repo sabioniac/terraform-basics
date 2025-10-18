2d787e7c8526dc73e14f8901eed2154c0d7da412
sabioniac <sabionweb@gmail.com>
2025-10-18 19:24:48 +0000

    Modified with Ver and datasource

--- basic/basic-userdata/ec2-create.tf
+++ basic/basic-userdata/ec2-create.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "Ec2demo" {
-  ami           = "ami-0341d95f75f311023"
-  instance_type = "t3.micro"
+  ami           = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+  key_name = var.instance_keypair
   user_data     = file("${path.module}/app-install.sh")
   tags = {
     "Name"    = "EC2 Demo"
---
+++ basic/basic-userdata/ec2-securitygp.tf
@@ -0,0 +1,30 @@
+# Define AWS Security group
+resource "aws_security_group" "Allow-http-ssh" {
+  description = "Security group allow access on http and SSH"
+  name = "Allow-http-ssh"
+  ingress {
+    description = "Allow ssh access"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Http Access"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all outbound"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Allow Access"
+    Craeted = "Terraform"
+  }
+}
